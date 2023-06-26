c1d550c8dcc1a858bbdd6d6cc647af9a8ec77864
rzaidi10 <rzaidi@ucls.uchicago.edu>
2023-06-25 23:49:18 -0500

    Debugging

--- ResearchSorter.py
+++ ResearchSorter.py
@@ -1,25 +1,35 @@
+
 import pandas as pd
-from googlesearch import search
 import os
-import re
 from collections import defaultdict
+import requests
+import json
+
+# Your Mapbox API access token
+MAPBOX_TOKEN = 'pk.eyJ1IjoicnphaWRpIiwiYSI6ImNsamNkaWM1MzB1bmUzZHFyamh6ajUwN28ifQ.MfsDDOxgRYif0hAr3I94dw'
+
 
 def get_state(university):
     try:
-        # Google search
-        query = university + ' state'
-        for j in search(query, num=1, stop=1, pause=2):
-            # Extract state from search result
-            match = re.search(r'in ([\w ]+),', j)
-            if match:
-                return match.group(1)
+        # Prepare API request
+        url = f'https://api.mapbox.com/geocoding/v5/mapbox.places/{university}.json?access_token={MAPBOX_TOKEN}'
+        response = requests.get(url)
+        data = json.loads(response.text)
+
+        # Extract state from API response
+        if data['features']:
+            for feature in data['features']:
+                for context in feature['context']:
+                    if context['id'].startswith('region'):
+                        return context['text']
     except Exception as e:
-        print(e)
+        print(f'Error occurred while finding state for university {university}: {e}')
     return None
+
 
 # Load the xlsx file
 filepath = os.path.expanduser("~/Desktop/EconomicsPaper/HERDrankings.xlsx")
-df = pd.read_excel(filepath, header=None, skiprows=5, usecols=[0,1], names=["Institution", "R&D expenditures"])
+df = pd.read_excel(filepath, header=None, skiprows=5, usecols=[0, 1], names=["Institution", "R&D expenditures"])
 
 # Map universities to states and group R&D expenditures
 state_expenditures = defaultdict(int)
